5ba8dea89781458edbc20b67d30fb41cfcb1e2b6
delpinoc <jorgemartin.delpinoc@gmail.com>
2025-02-17 20:27:05 +0700

    Topic 9 - object destructuring finished

--- 09-Data-Structures-Operators/assignments.js
+++ 09-Data-Structures-Operators/assignments.js
@@ -219,3 +219,67 @@ console.log("oneStarRatings: ", oneStarRatings);
 console.log("threeStarRatings: ", threeStarRatings);
 
 // DESTRUCTURING OBJECTS
+
+// 2.1
+// Destructure the first book object from the books array into variables
+// called title, author and ISBN.
+
+const {title, author, ISBN} = books[0];
+console.log("\ntitle: ", title);
+console.log("author: ", author);
+console.log("isbn: ", ISBN);
+
+// 2.2
+// Each book object has the keywords property. Destructure the first book
+// object from the books array into a variable called tags. The tags variable
+// should be assigned with the value of the keywords property.
+
+const {keywords: tags} = books[0];
+console.log("\ntags: ", tags);
+
+// 2.3
+// The seventh book from the books array is missing the programmingLanguage
+// property. Destructure the seventh book object (books[6]) into variables
+// called language and programmingLanguage. Assign the programmingLanguage
+// variable with a default value of 'unknown'.
+
+const {language, programmingLanguage = "unknown"} = books[6];
+console.log("\nlanguage: ", language);
+console.log("programmingLanguage: ", programmingLanguage);
+
+// 2.4
+// Below are two variables called bookTitle and bookAuthor.
+// Reassign them with the values of the title and author properties of
+// the first book object from the books array.
+
+let bookTitle = "unknown";
+let bookAuthor = "unknown";
+
+({title: bookTitle, author: bookAuthor} = books[0]);
+console.log("\nbookTitle: ", bookTitle);
+console.log("bookAuthor: ", bookAuthor);
+
+// 2.5
+// Each book object has a deeply nested rating property as illustrated below
+
+const {thirdParty: {goodreads: {rating: bookRating}}} = books[0];
+console.log("\nbookRating: ", bookRating);
+
+// 2.6
+// Write a function called printBookInfo that has three parameters
+// called title, author and year. This function should work for a
+// single object passed as an argument, and it should log to
+// the console information about the book
+// in this format: "${title} by ${author}, ${year}".
+
+// If year is undefined (was not passed), it should be assigned with
+// a default value of 'year unknown'.
+
+function printBookInfo({title, author, year = "year unknown"}){
+    console.log(`"${title} by ${author}, ${year}"`);
+}
+
+
+printBookInfo({title: "Algorithms", author: "Robert Sedgewick", year: "2011"});
+printBookInfo({ title: 'Algorithms', author: 'Robert Sedgewick' });
+
--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -1,36 +1,49 @@
 'use strict';
 
 // Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+const flights = '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
 const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  order: function(numberCategoria, startMenu, endMenu) {
-    return [this.categories[numberCategoria- 1], this.starterMenu[startMenu - 1], this.mainMenu[endMenu - 1]];
-  },
+    openingHours: {
+        thu: {
+            open: 12,
+            close: 22,
+        },
+        fri: {
+            open: 11,
+            close: 23,
+        },
+        sat: {
+            open: 0, // Open 24 hours
+            close: 24,
+        },
+    },
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
+    order: function (startedIndex, mainIndex) {
+        return [this.starterMenu[startedIndex], this.mainMenu[mainIndex]];
     },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
+
+    orderDelivery: function({starterIndex = 1, mainIndex = 0, time = "20:00", address}){
+        console.log(starterIndex, mainIndex, time, address);
+    }
 };
+
+
+
+/*
+restaurant.orderDelivery({time: "22:30", address: "via del sole, 21", mainIndex: 2, starterIndex: 2,})
+
+restaurant.orderDelivery({
+    address: "Via Angelo Tavanti",
+    starterIndex: 1,
+})
 
 
 // console.log(restaurant)
@@ -81,3 +94,57 @@ console.log("Tercero Dos: ", terceroDos);
 // Default values
 const [p, q, r = 0] = [8, 9];
 console.log(p, q, r);
+
+const {name, openingHours, categories} = restaurant;
+console.log(name, openingHours, categories);
+
+const {name: restaurantName, openingHours: hours, categories: tags} = restaurant;
+console.log("\n", restaurantName, hours, tags);
+
+
+// Default values
+const {menu = [], starterMenu: starters = []} = restaurant;
+console.log(menu, starters);
+
+// Mutating variables
+let a = 111;
+let b = 999;
+const obj = {a: 23, b: 7, c: 14};
+
+({a, b} = obj);
+console.log(a, b);
+
+// Nested Objects
+const {fri: {open: o, close: c}} = openingHours;
+console.log(o, c);
+
+ */
+
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+
+console.log(...newArr);
+console.log(1, 2, 7, 8, 9);
+
+const newMenu = [...restaurant.mainMenu, "Gnocci"];
+console.log(newMenu);
+console.log(...newMenu);
+
+// Copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+console.log(mainMenuCopy);
+
+// Join 2 arrays
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu)
+
+// Iterables: arrays, strings, maps, sets. NOT objects
+
+const cadena = "Hola me llamo jorge";
+const newCadena = [...cadena, " :-)"];
+console.log(...newCadena);
+
